0b0b57affc8bad57faf7ec099b859781ea86e0f8
Flatkaka <hilmarpall@hotmail.com>
2018-09-18 11:14:05 +0000

    both

--- Tiletravel.py
+++ Tiletravel.py
@@ -2,18 +2,18 @@ x = 1
 y = 1
 while True:
     if x == 1 and y == 1:
-        print('You can tavel: (N)orth')
+        print('You can travel: (N)orth.')
         att = input('Direction: ')
         if att != 'n' and att != 'N':
             print('Not a valid direction!')
-            continue
+            att = input('Direction: ')
         y += 1
     if x== 1 and y == 2:
-        print('You can tavel: (N)orth or (E)ast or (S)outh.')
+        print('You can travel: (N)orth or (E)ast or (S)outh.')
         att = input('Direction: ')
         if att == 'w' or att == 'W':
             print('Not a valid direction!')
-            continue
+            att = input('Direction: ')
         if att == 'n' or att == 'N':
             y += 1
         elif att == 'e' or att == 'E':
@@ -21,63 +21,63 @@ while True:
         else:
             y -= 1
     if x== 2 and y == 2:
-        print('You can tavel: (S)outh or (W)est.')
+        print('You can travel: (S)outh or (W)est.')
         att = input('Direction: ')
         if att != 'w' and att != 'W' and  att != 's' and att != 'S':
             print('Not a valid direction!')
-            continue
+            att = input('Direction: ')
         if att == 'w' or att == 'W':
             x -= 1
         else:
             y -= 1
     if x== 2 and y == 1:
-        print('You can tavel: (N)orth.')
+        print('You can travel: (N)orth.')
         att = input('Direction: ')
         if att != 'n' and att != 'N':
             print('Not a valid direction!')
-            continue
+            att = input('Direction: ')
         y += 1
     if x== 1 and y == 3:
-        print('You can tavel: (E)ast or (S)outh.')
+        print('You can travel: (E)ast or (S)outh.')
         att = input('Direction: ')
         if att != 'e' and att != 'E' and  att != 's' and att != 'S':
-            print('Not a valid direction')
-            continue
+            print('Not a valid direction!')
+            att = input('Direction: ')
         if att == 'e' or att == 'E':
             x += 1
         else:
             y -= 1
     if x== 2 and y == 3:
-        print('You can tavel: (E)ast or (W)est.')
+        print('You can travel: (E)ast or (W)est.')
         att = input('Direction: ')
         if att != 'e' and att != 'E' and  att != 'w' and att != 'W':
-            print('Not a valid direction')
-            continue
+            print('Not a valid direction!')
+            att = input('Direction: ')
         if att == 'e' or att == 'E':
             x += 1
         else:
             x -= 1
     if x== 3 and y == 3:
-        print('You can tavel: (S)outh or (W)est.')
+        print('You can travel: (S)outh or (W)est.')
         att = input('Direction: ')
         if att != 'w' and att != 'W' and  att != 's' and att != 'S':
-            print('Not a valid direction')
-            continue
+            print('Not a valid direction!')
+            att = input('Direction: ')
         if att == 'w' or att == 'W':
             x -= 1
         else:
             y -= 1
     if x== 3 and y == 2:
-        print('You can tavel: (N)orth or (S)outh.')
+        print('You can travel: (N)orth or (S)outh.')
         att = input('Direction: ')
         if att != 'n' and att != 'N' and  att != 's' and att != 'S':
-            print('Not a valid direction')
-            continue
+            print('Not a valid direction!')
+            att = input('Direction: ')
         if att == 'n' or att == 'N':
             y += 1
         else:
             y -= 1
     if x == 3 and y == 1:
-        print('Vitctory!')
+        print('Victory!')
         break
 
---
+++ tiletraveler.py
@@ -0,0 +1,79 @@
+x = 1
+y = 1
+def travel (main):
+    print('You can travel:', main + '.')
+def direction ():
+    att = input('Direction: ')
+    return att
+def invalid (att, main):
+    for n in main:
+        if n.isupper():
+            if att == n.lower() or att == n:
+                break
+    else:
+        print('Not a valid direction!')
+        att = input('Direction: ')
+def move (att,x,y):
+    if att == 'n' or att == 'N':
+        y += 1
+    elif att == 's' or att == 'S':
+        y -= 1
+    elif att == 'e' or att == 'E':
+        x += 1
+    else:
+        x -= 1
+    return x,y
+
+while True:
+    if x == 1 and y == 1:
+        main = '(N)orth'
+        travel(main)
+        att = direction()
+        invalid(att, main )
+        x,y = move(att, x, y)
+    if x== 1 and y == 2:
+        main = '(N)orth or (E)est or (S)outh'
+        travel(main)
+        att = direction()
+        invalid(att, main )
+        x,y = move(att, x, y)
+    if x== 2 and y == 2:
+        main = '(S)outh or (East)'
+        travel(main)
+        att = direction()
+        invalid(att, main )
+        x,y = move(att, x, y)
+    if x== 2 and y == 1:
+        main = '(N)orth'
+        travel(main)
+        att = direction()
+        invalid(att, main )
+        x,y = move(att, x, y)
+    if x== 1 and y == 3:
+        main = '(E)est or (S)outh'
+        travel(main)
+        att = direction()
+        invalid(att, main )
+        x,y = move(att, x, y)
+    if x== 2 and y == 3:
+        main = '(E)est or (W)est'
+        travel(main)
+        att = direction()
+        invalid(att, main )
+        x,y = move(att, x, y)
+    if x== 3 and y == 3:
+        main = '(S)outh or (W)est'
+        travel(main)
+        att = direction()
+        invalid(att, main )
+        x,y = move(att, x, y)
+    if x== 3 and y == 2:
+        main = '(N)orth or (S)outh'
+        travel(main)
+        att = direction()
+        invalid(att, main )
+        x,y = move(att, x, y)
+    if x == 3 and y == 1:
+        print('Victory!')
+        break
+
